d8c5b30d40aa07709481c6d7d61f1710b4137448
mohitg008 <mohitgaikwadxp@gmail.com>
2024-09-22 19:45:05 +0530

    new JS array methods

---
+++ JavaScript/asynchronous.js
@@ -0,0 +1,10 @@
+/*setTimeout(() => {
+    console.log("Hello console")
+},5000)*/
+
+console.log("start");
+var id = setInterval(()=>{
+    console.log("delay of 2 second")
+},2000)
+
+console.log("stop");
