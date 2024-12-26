fdd40043f0773d7072328850d6f6cfa361b92a7c
mohitg008 <mohitgaikwadxp@gmail.com>
2024-12-26 23:40:48 +0530

    basic css

---
+++ JavaScript/practice/basicQuestions.js
@@ -0,0 +1,28 @@
+// const arr1 = [1,2,3,4];
+// const arr2 = [5,6,7,8];
+// /*var mergeArr = [...arr1,...arr2];
+// console.log(mergeArr)*/
+
+// const mergedArr = [...arr1];
+// for(let i=0;i<arr2.length;i++){
+//     mergedArr.push(arr2[i]);
+// }
+
+// console.log(mergedArr);
+
+
+/* const arr = [[1,2],[3,4],[5,6]];
+
+var flatArr = [];
+
+for(let i=0;i<arr.length;i++){
+   
+    for(let j=0;j<arr[i].length;j++){
+        flatArr.push(arr[i][j]);
+    }
+}
+console.log(flatArr); */
+
+
+
+
