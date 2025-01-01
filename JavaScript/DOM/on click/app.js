baa6d65390671f4b7457dc97294d775863a8af38
mohitg008 <mohitgaikwadxp@gmail.com>
2025-01-01 22:29:53 +0530

    event listener

--- JavaScript/DOM/on click/app.js
+++ JavaScript/DOM/on click/app.js
@@ -15,3 +15,27 @@ btn.onmouseenter = scream;
 document.querySelector('h1').onclick = () => {
     alert('you clciked the h1!');
  }
+
+ var btn3 = document.querySelector('#v3');
+
+ btn3.addEventListener('dblclick',() =>{
+    alert('YOU CLICKED ME!!!');
+ })
+
+
+function twist(){
+    console.log('TWIST');
+}
+
+function shout(){
+    console.log('SHOUT');
+}
+
+var btn4 = document.querySelector('#v4');
+
+btn4.addEventListener('click',twist,{once:true});
+btn4.addEventListener('click',shout);
+
+/* btn4.onclick = twist;
+btn4.onclick = shout; */
+
