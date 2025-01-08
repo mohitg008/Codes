8083b1f8c75d2faadf426ec85fd511cb5651157b
mohitg008 <mohitgaikwadxp@gmail.com>
2025-01-09 03:04:47 +0530

    async keyword new code

--- JavaScript/Async JS/app.js
+++ JavaScript/Async JS/app.js
@@ -1,6 +1,19 @@
-console.log('Sending request to server!');
+/* console.log('Sending request to server!');
 
 setTimeout(()=> {
    console.log('Here is your data from the server...')
 },3000)
-console.log('I am at the end of the file!');
+console.log('I am at the end of the file!'); */
+
+/* async function hello(){
+   
+} */
+
+
+const sing = async () => {
+   return 'LA LA LA';
+}
+
+sing().then((data)=>{
+   console.log('promise resolved with:', data);
+})
